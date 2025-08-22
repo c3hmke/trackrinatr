669e995c53fb00dd6bf6e2414df01eba401652cf
Caius Ehmke <caius@bit.nl>
2025-08-22 16:45:36 +0200

    Add a Theme file for the application; Apply to screens

--- lib/widgets/gradient_background.dart
+++ lib/widgets/gradient_background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trackrinatr/app/theme.dart';
 
 class GradientBackground extends StatelessWidget {
   final Widget child;
@@ -7,13 +8,7 @@ class GradientBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFF1e3a8a), Color(0xFF4c1d95), Color(0xFF312e81)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
+      decoration: const BoxDecoration(gradient: AppColors.gradient),
       child: child,
     );
   }
