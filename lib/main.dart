18f08da8c94a7f9ded84947c4ed94605a088ca32
Caius Ehmke <caius@bit.nl>
2025-08-27 14:04:50 +0200

    Add hive + hive_flutter dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:trackrinatr/app/theme.dart';
 import 'package:trackrinatr/screens/home_screen.dart';
 
-void main() => runApp(const App());
+void main() async {
+  await Hive.initFlutter();
+
+  // TODO: write these adapters
+  // Hive.registerAdapter(ExerciseAdapter());
+  // Hive.registerAdapter(WorkoutAdapter());
+  //
+  // await Hive.openBox<Exercise>('exercises');
+  // await Hive.openBox<Workout>('workouts');
+
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({super.key});
